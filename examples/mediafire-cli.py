f37db1741c3865ae7ea74dd8e58b2a3be7848042
Paulo Garcia <pgarcia@qnx.com>
2016-02-09 22:30:20 -0500

    added app-id parameter. assumes APP_ID variable by default

--- examples/mediafire-cli.py
+++ examples/mediafire-cli.py
@@ -152,14 +152,8 @@ def main():  # pylint: disable=too-many-statements
                         default=os.environ.get('MEDIAFIRE_EMAIL', None))
     parser.add_argument('--password', dest='password', required=False,
                         default=os.environ.get('MEDIAFIRE_PASSWORD', None))
-<<<<<<< HEAD
+
     parser.add_argument('--appid', dest='appid', required=False, default=APP_ID)
-=======
-    parser.add_argument('--appid', dest='appid', required=False,
-                        default=os.environ.get('MEDIAFIRE_APPID', None))
-    parser.add_argument('--apikey', dest='apikey', required=False,
-                        default=os.environ.get('MEDIAFIRE_API_KEY', None))
->>>>>>> 1faeb6737edaeb7c5050bb2b6b5b573d9f6e9cc3
 
     actions = parser.add_subparsers(title='Actions', dest='action')
     # http://bugs.python.org/issue9253#msg186387
